4c5c5a9c8f0f1b9196c267ada70d8fbdbb9118be
ChaitanyaChaitu <chaitanyavantaku@gmail.com>
2024-01-27 18:29:25 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-#This is my Gitlab first project
+# GitLab-CiCd
--- test.py
+++
@@ -1,9 +0,0 @@
-def add(a,b):
-    return a+b
-
-def sub(a,b):
-    return a-b
-result = add(1,2)
-result2 = sub(3,2)
-print('The addition is'result)
-print('The subtraction is'result2)
